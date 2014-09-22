fb8e184f0706772c4d17347517a5c3a7fe2a1545
Kevin Swiber <kswiber@gmail.com>
2014-09-22 16:43:46 -0400

    Initial commit.

---
+++ ZettaCloud/ZettaCloud/server.js
@@ -0,0 +1,14 @@
+﻿var zetta = require('zetta');
+
+var PORT = process.env.PORT || 3000;
+
+zetta()
+  .name('cloud')
+  .expose('*')
+  .listen(PORT, function (err) {
+    if (err) {
+        console.error(err);
+        process.exit(1);
+    }
+    console.log('running on http://localhost:', PORT)
+});
